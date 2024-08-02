7de513f72b4c19b2dfda892b744390e728fa1f09
btwtill <57750780+btwtill@users.noreply.github.com>
2024-08-02 13:43:42 +0200

    file clean up

--- projectSetupManager.py
+++
@@ -1,7 +0,0 @@
-
-class pipelineProjectSetupManager():
-    def __init__(self, projectPath, projectName) -> None:
-        self.projectPath = projectPath
-
-    def setupNewProject(self):
-        pass
